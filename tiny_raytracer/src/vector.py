9a407de7e0a994e113ba955358198ddda188ef66
Philipp <siebesiech@gmail.com>
2021-09-27 22:29:56 +0200

    Added properties x, y, z, w

--- tiny_raytracer/src/vector.py
+++ tiny_raytracer/src/vector.py
@@ -11,6 +11,35 @@ class Vector:
     def __str__(self):
         return str(self.components)
 
+    @property
+    def x(self):
+        return self.components[0]
+
+    @property
+    def y(self):
+        assert self.size >= 2
+        return self.components[1]
+
+    @property
+    def z(self):
+        assert self.size >= 3
+        return self.components[2]
+
+    @property
+    def w(self):
+        assert self.size >= 4
+        return self.components[3]
+
+    def dot(self, other):
+        scalar_product = 0
+        assert self.size == other.size
+        for i in range(self.size):
+            scalar_product += self.components[i] * other.components[i]
+        return scalar_product
+
+    def norm(self):
+        return math.sqrt(self[0] * self[0] + self[1] * self[1] + self[2] * self[2])
+
     def cross(self, rhs):
         assert self.size == 3 and rhs.size == 3
         return Vector(
@@ -54,16 +83,6 @@ class Vector:
         for i in range(self.size):
             new_vec.append(self.components[i] + other.components[i])
         return Vector(*new_vec)
-
-    def dot(self, other):
-        scalar_product = 0
-        assert self.size == other.size
-        for i in range(self.size):
-            scalar_product += self.components[i] * other.components[i]
-        return scalar_product
-
-    def norm(self):
-        return math.sqrt(self[0] * self[0] + self[1] * self[1] + self[2] * self[2])
 
     def __eq__(self, other):
         if len(self.components) != other.size:
